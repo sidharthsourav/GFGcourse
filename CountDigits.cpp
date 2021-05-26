60d42df2dd45324db5ed70262dc402bfc9128eca
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-05-26 19:37:48 +0530

    Naive apprach added.

--- CountDigits.cpp
+++ CountDigits.cpp
@@ -1,9 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Most Efficient method
 int CountDigits(long n)
 {
     return floor(log10(n)+1);
+}
+// Naive approach
+int DigitCount(int n)
+{
+    int count=0;
+    while(n!=0)
+    {
+        n=n/10;
+        count++;
+    }
+    return count;
 }
 int main()
 {
