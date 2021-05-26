275d13818d06e6401ade40a60d9115678bce7286
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-05-26 20:33:53 +0530

    Count trailing zeros function added.

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int IterativeFactorial(int n)
+long long IterativeFactorial(int n)
 {
-    int result=1;
+    long long result=1;
     for(int i=2;i<=n;i++) result=result*i;
     return result;
 }
@@ -12,9 +12,17 @@ int RecursiveFactorial(int n)
     if (n==0) return 1;
     return n*RecursiveFactorial(n-1);
 }
+// effetient approach O(logn) and doesn't give the buffer overflow problem
+int CountTrailingZeros(int n)
+{
+    int res=0;
+    for(int i=5;i<=n;i=i*5) res=res+(n/i);
+    return res;
+}
 
 int main()
 {
-    cout<<IterativeFactorial(5)<<" ";
-    cout<<RecursiveFactorial(5);
+    cout<<IterativeFactorial(15)<<" ";
+    cout<<RecursiveFactorial(5)<<" ";
+    cout<<CountTrailingZeros(15);
 }
