b3924b05a7e06174b1bac28ea3f7713fd3973e40
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-05-26 23:33:40 +0530

    more optimized approach added.

--- primenumber.cpp
+++ primenumber.cpp
@@ -8,6 +8,19 @@ bool isPrime(int n)
         if(n%i==0) return false;
     return true;    
 }
+// more optimized solution 
+bool IsPrime(int n)
+{
+    if(n==1) return false;
+    if(n==2 || n==3) return true;
+    if(n%2==0 || n%3==0) return false;
+    for(int i=5;i*i<=n;i+6)
+    {
+        if(n%i==0 ||n%(i+2)==0) return false;
+    }
+    return true;
+
+}
 int main()
 {
     cout<<isPrime(17);
