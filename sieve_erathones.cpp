fe07a4737efafa30ec147ff874189dbf6c70674b
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-05-31 15:46:26 +0530

    completed the Function.

--- sieve_erathones.cpp
+++ sieve_erathones.cpp
@@ -1,9 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-void IsPrime(int n);
-
+  
+  // method to print all prime numbers beetween 0 and N with time complexity of O(nloglogn)
 void seive(int n)
 {
     vector <bool> primeArray(n+1,true);
+    for(int i=2;i<=n;i++)
+    {
+        if(primeArray[i])
+        {
+            cout<<i<<" ";
+            for(int j=i*i;j<=n;j=j+i) primeArray[j]=false;
+        }
+    }
+}
+int main()
+{
+    seive(51);
 }
