88d3cc1bedadd7df53fb387e48813fb5e09f12b3
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-06-12 22:22:43 +0530

    array list implementation in java

---
+++ Arrays/main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class main
+{
+    public static void main(String args[])
+    {
+        ArrayList <Integer> a = new ArrayList<Integer>();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        System.out.println(a);
+
+
+    }
+}
