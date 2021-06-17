f36e4773f50ce7babefb4ee10b5441331d11bd40
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-06-18 01:51:11 +0530

    efficient approah added.

--- Arrays/left_rotate_array.cpp
+++ Arrays/left_rotate_array.cpp
@@ -10,10 +10,33 @@ void left_rotate(int arr[],int d,int size)
 
 }
 
+// more efficient solution in time complexity of O(n) and space complexity O(1)
+//main idea is reverse til d elements first then reverse rest elements
+// then reverse the whole array
+
+void reverse(int arr[],int low,int high)
+{
+    while(low<high)
+    {
+        swap(arr[low],arr[high]);
+        low++;
+        high--;
+    }
+}
+void rotate(int arr[],int d,int size)
+{
+    reverse(arr,0,d-1);
+    reverse(arr,d,size-1);
+    reverse(arr,0,size-1);
+
+}
+
+
 int main()
 {
     int a[]={1,2,3,4,5,6,7,8,9};
     int size=sizeof(a)/sizeof(a[0]);
-    left_rotate(a,3,size);
+    //left_rotate(a,3,size);
+    rotate(a,3,size);
     for(int i=0;i<size;i++) cout<<a[i]<<" ";
 }
