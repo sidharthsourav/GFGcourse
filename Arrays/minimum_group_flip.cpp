de4ba941244112dc0d5a70b4b2542697fa826eda
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-06-21 23:23:43 +0530

    minimum group flips added.

---
+++ Arrays/minimum_group_flip.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int print_groups(int arr[],int size)
+{
+    for(int i=1;i<size;i++)
+    {
+        if(arr[i]!=arr[i-1])
+        {
+            if(arr[i]!=arr[0]) cout<<"From : "<<i<<" to ";
+            else cout<<(i-1)<<endl;
+        }
+    }
+    if (arr[size-1]!=arr[0]) cout<<(size-1)<<endl;
+}
+int main()
+{
+    int array[]={1,0,1,0,1,1,1,1,1,0,0,0,1,0,1,0,1};
+    int size=sizeof(array)/sizeof(array[0]);
+    print_groups(array,size);
+}
