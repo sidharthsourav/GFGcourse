5822c7be3144ec7d1aa9194020aaa7899a9072e8
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-06-22 01:31:19 +0530

    subarray sum checker function added.
    The function checks for availble subarray equiavalent to sum it automatically changes window size to find the answer.

--- Arrays/window_sliding.cpp
+++ Arrays/window_sliding.cpp
@@ -17,10 +17,32 @@ int max_subarray_sum(int arr[],int size,int k) // k=window size
     }
     return max_sum;
 }
+// finding a subarray whose sum is provided using window sliding technique
+// this works only on positive array elements array consists of positive values only
+bool subarray_sum_checker(int arr[],int size,int sum)
+{
+    int current_sum=arr[0],start=0;
+    for(int i=1;i<=size;i++)
+    {
+        while(current_sum>sum && start<i-1)
+        {
+            current_sum-=arr[start];
+            start++;
+        }
+        if(current_sum==sum) return true;
+        if(i<size) current_sum+=arr[i];
+
+    }
+    return (current_sum==sum);
+}
+
 int main()
 {
     int array[]={12,-89,2,56,89,-23,41,-55};
     int size=sizeof(array)/sizeof(array[0]);
     int max_sum=max_subarray_sum(array,size,3);
-    cout<<max_sum;    
+    int array1[]={12,89,2,56,89,23,41,55};
+    int size1=sizeof(array)/sizeof(array[0]);   
+    bool res=subarray_sum_checker(array1,size1,147) ;
+    cout<<max_sum<<" "<<res;    
 }
