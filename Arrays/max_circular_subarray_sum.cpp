7bcebed2ee199070588ead8b604dfbe821e411ab
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2021-06-21 18:37:43 +0530

    max sum in circular sub array completed.

--- Arrays/max_circular_subarray_sum.cpp
+++ Arrays/max_circular_subarray_sum.cpp
@@ -1,13 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int max_subarray_sum(int arr[],int size)
+{
+    int res=arr[0];
+    int max_ending=arr[0];
+    for(int i=1;i<size;i++)
+    {
+        max_ending=max(arr[i],max_ending+arr[i]);
+        res=max(res,max_ending);
+    }
+    return res;
+}
 int sum_subarray(int arr[],int size)
 {
-
+    int normal_sum=max_subarray_sum(arr,size);
+    if(normal_sum<0) return normal_sum;
+    int array_sum=0;
+    for(int i=0;i<size;i++)
+    {
+        array_sum=array_sum +arr[i];
+        arr[i]=-arr[i];
+    }
+    int circular_sum=array_sum+max_subarray_sum(arr,size);
+    return max(normal_sum,circular_sum);
 }
 int main()
 {
-    int array[]={12,-89,2,56,89,-23,41,-55};
+    int array[]={8,-4,3,-5,4};
     int size=sizeof(array)/sizeof(array[0]);
     int res=sum_subarray(array,size);
     cout<<res;
