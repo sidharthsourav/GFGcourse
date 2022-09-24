2feb59765684b16be843136809c619b9e9955994
sidharthsourav <63449305+sidharthsourav@users.noreply.github.com>
2022-09-24 23:32:17 +0530

    few files addes.

---
+++ Bit Manipulation/test.java
@@ -0,0 +1,14 @@
+public class test
+{
+    public static void main(String[] args)
+    {
+        String s= "hello World";
+        for(int i=0;i<4;i++)
+        {
+            int x=s.length()-1;
+            s=s.substring(0, x);
+        }
+        System.out.println(s);
+        boolean
+    }
+}
